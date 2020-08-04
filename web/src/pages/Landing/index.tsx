f596ddac489a0f3a6d5c72b9605cf731107356de
Matheus Ricardo Brunelli <matheus.brunelli@gmail.com>
2020-08-04 07:00:06 -0300

    adiciona layout responsivo na landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -21,15 +21,15 @@ const Landing = () => {
                     className="hero-image"
                 />
 
-                <div className="button-container">
+                <div className="buttons-container">
                     <a href="" className="study">
                         <img src={studyIcon} alt="Estudar" />
                         Estudar
                     </a>
 
                     <a href="" className="give-classes">
-                        <img src={giveClassesIcon} alt="Estudar" />
-                        Estudar
+                        <img src={giveClassesIcon} alt="Dar aulas" />
+                        Dar aulas
                     </a>
                 </div>
 
